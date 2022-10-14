19b0b9effec02ee3a05a228e094a525ae3a312bf
deltemp <deltemp@hotmail.com>
2022-10-14 18:32:49 -0300

    ignite-labs after

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import './styles/global.css';
+import { Envelope, Lock } from "phosphor-react";
+import { Button } from "./components/Button";
+import { Checkbox } from "./components/Checkbox";
+import { Heading } from "./components/Heading";
+import { Text } from "./components/Text";
+import { TextInput } from "./components/TextInput";
+import { Logo } from "./Logo";
+import { SignIn } from "./pages/SignIn";
+
+import "./styles/global.css";
 
 export function App() {
   return (
-    <h1 className="font-bold text-2xl text-cyan-300">Hello World</h1>
+    <SignIn />
   )
 }
